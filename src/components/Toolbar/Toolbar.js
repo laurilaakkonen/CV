c33eddc86950dcb3a8ad08625164e8e6b0d596c8
Lauri Laakkonen <laulaakk@gmail.com>
2020-01-22 18:37:10 +0200

    FIX href to link

--- src/components/Toolbar/Toolbar.js
+++ src/components/Toolbar/Toolbar.js
@@ -1,24 +1,29 @@
 import React from 'react'
 
+import {
+  HashRouter as Router,
+  Link
+} from "react-router-dom";
+
 import './Toolbar.css'
 
 const toolbar = props => (
   <header className="toolbar">
+  <Router>
   <nav className="toolbar_navigation">
     <div className="toolbar_logo"><a href="/">Lauri Laakkonen</a></div>
     <div className="toolbar_logo_bonus">the web app CV</div>
     <div className="space" />
     <div className="toolbar_navigation-items">
       <ul>
-        <li><a href="/skills_and_experience">Skills and experience</a></li>
-        <li><a href="/previous_work">Previous work</a></li>
-        <li><a href="/contact">Contact me</a></li>
+        <li><Link to="/skills_and_experience">Skills and experience</Link></li>
+        <li><Link to="/previous_work">Previous work</Link></li>
+        <li><Link to="/contact">Contact me</Link></li>
       </ul>
     </div>
   </nav>
+  </Router>
   </header>
 );
 
 export default toolbar;
-
-    {/*<Link to="/contact">Contact me</Link></li>*/}
