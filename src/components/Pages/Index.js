5539a5907dd2cea5bd22298ee08253db44ec3450
Lauri Laakkonen <laulaakk@gmail.com>
2020-01-23 15:29:22 +0200

    ADD main page content

--- src/components/Pages/Index.js
+++ src/components/Pages/Index.js
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import lauripic from './Lauripic.jpg'
+import './Index.css'
 
 const index = props => (
   <div>
   hello I am index
+  <div className="parallel">
+    <div>
+    <h2>Header</h2>
+    I am text next to picture
+    </div>
+      <div className="lauripic">
+          <img src={lauripic} width="380" height="485"/>
+      </div>
+  </div>
   </div>
 );
 
